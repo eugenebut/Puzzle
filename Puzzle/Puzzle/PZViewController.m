94cd76bcfb4cf401c1f50f2b15dfa30534c615a3
eugenebut <eugenebut@Eugenes-MacBook-Pro.local>
2012-06-28 23:12:35 -0700

    Implemented tiles adding

--- Puzzle/Puzzle/PZViewController.m
+++ Puzzle/Puzzle/PZViewController.m
@@ -8,6 +8,8 @@
 
 #import "PZViewController.h"
 #import "PZPuzzle.h"
+#import "PZTile.h"
+#import <QuartzCore/QuartzCore.h>
 
 static const NSUInteger kPuzzleSize = 4;
 
@@ -18,11 +20,13 @@ static const NSUInteger kPuzzleSize = 4;
 @end
 
 @implementation PZViewController
+@synthesize  puzzle, puzzleImageFile;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    [self addTiles];
 }
 
 - (void)viewDidUnload
@@ -40,10 +44,46 @@ static const NSUInteger kPuzzleSize = 4;
 {
     if (nil == puzzle)
     {
+        // TODO: restore state
         UIImage *image = [[UIImage alloc] initWithContentsOfFile:self.puzzleImageFile];
         puzzle = [[PZPuzzle alloc] initWithImage:image size:kPuzzleSize];
     }
     return puzzle;
+}
+
+- (void)addTiles
+{
+    CGFloat tileWidth = [self tileWidth];
+    CGFloat tileHeight = [self tileHeight];
+
+    for (NSUInteger x = 0; x < kPuzzleSize; x++)
+    {
+        for (NSUInteger y = 0; y < kPuzzleSize; y++)
+        {
+            CALayer *tileLayer = [CALayer new];
+            PZTile *tile = [self.puzzle tileAtLocation:PZTileLocationMake(x, y)];
+            tileLayer.contents = (id)[tile.image CGImage];
+            tile.representedObject = tileLayer;
+            tileLayer.frame = CGRectInset(CGRectMake(x * tileWidth, y * tileHeight,
+                                                     tileWidth, tileHeight), 1.0, 1.0);
+            [self.view.layer addSublayer:tileLayer];
+        }
+    }
+}
+
+- (PZTileLocation)tileLocationAtPoint:(CGPoint)aPoint
+{
+    return PZTileLocationMake(aPoint.x * kPuzzleSize / [self tileWidth], aPoint.y * kPuzzleSize / [self tileHeight]);
+}
+
+- (CGFloat)tileWidth
+{
+    return [UIScreen mainScreen].applicationFrame.size.width / kPuzzleSize;
+}
+
+- (CGFloat)tileHeight
+{
+    return [UIScreen mainScreen].applicationFrame.size.height / kPuzzleSize;
 }
 
 @end
