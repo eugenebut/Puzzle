177dcca6d618a0f62f8164987c5049e78c52d58e
eugenebut <eugenebut@Eugenes-MacBook-Pro.local>
2012-07-01 13:16:02 -0700

    Added Defauls screen.

---
+++ Puzzle/PuzzleTests/PuzzleTests.m
@@ -0,0 +1,32 @@
+//
+//  PuzzleTests.m
+//  PuzzleTests
+//
+//  Created by Eugene But on 6/27/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "PuzzleTests.h"
+
+@implementation PuzzleTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in PuzzleTests");
+}
+
+@end
