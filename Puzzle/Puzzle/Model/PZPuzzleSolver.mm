75153bd1ad4811375f4dac15ab6f15f02342db46
eugenebut <eugenebut@Eugenes-MacBook-Pro.local>
2012-10-13 23:06:26 -0700

    Optimised memory usage by Puzzle Solver

--- Puzzle/Puzzle/Model/PZPuzzleSolver.mm
+++ Puzzle/Puzzle/Model/PZPuzzleSolver.mm
@@ -27,21 +27,21 @@ public:
     inline puzzle_node(PZPuzzle *puzzle);
     
     // neighbour node
-    inline puzzle_node(const puzzle_node *node, char empty_tile);
+    inline puzzle_node(puzzle_node *node, uint8_t empty_tile);
     inline ~puzzle_node();
     
     inline void enqueue_neighbours(std::priority_queue<puzzle_node *, std::vector<puzzle_node *>,
-                            puzzle_node::comparator > *queue) const;
+                            puzzle_node::comparator > *queue);
 
     inline bool is_win() const {
         return 0 == manhattan_;
     }
     
-    inline char empty_tile() const {
+    inline uint8_t empty_tile() const {
         return empty_tile_;
     }
 
-    inline char manhattan() const {
+    inline uint8_t manhattan() const {
         return manhattan_;
     }
 
@@ -49,7 +49,7 @@ public:
         return weight_;
     }
 
-    inline const puzzle_node *previous_node() const {
+    inline puzzle_node *previous_node() const {
         return previous_node_;
     }
 
@@ -64,24 +64,24 @@ public:
     static const size_t kTilesCount = kPuzzleSize * kPuzzleSize;
 
 protected:
-    inline char tile_index(PZTileLocation location) const;
-    inline char calculate_manhatten() const;
+    inline uint8_t tile_index(PZTileLocation location) const;
+    inline uint8_t calculate_manhatten() const;
 
 private:
     
-    const puzzle_node *previous_node_;
+    puzzle_node *previous_node_;
+    uint8_t tiles_[kTilesCount];
+    uint8_t refcount_;
+    uint8_t empty_tile_;
+    uint8_t manhattan_;
     uint16_t move_;
     uint16_t weight_;
-    char empty_tile_;
-    char tiles_[kTilesCount];
-    char manhattan_;
-    char *refcount_;
 };
 
 puzzle_node::puzzle_node()
 : previous_node_(NULL),
   empty_tile_(100),
-  refcount_(new char(0)) {
+  refcount_(0) {
     ::memset(tiles_, 0x00, sizeof(tiles_));
 }
 
@@ -89,9 +89,9 @@ puzzle_node::puzzle_node(PZPuzzle *puzzle) :
     previous_node_(new puzzle_node()),
     empty_tile_(tile_index(puzzle.emptyTileLocation)),
     move_(0),
-    refcount_(new char(0))
+    refcount_(0)
 {
-    ++(*(previous_node_->refcount_));
+    ++(previous_node_->refcount_);
     [[puzzle allTiles] enumerateObjectsUsingBlock:^(id<IPZTile> aTile, NSUInteger anIndex, BOOL *aStop) {
         tiles_[anIndex] = tile_index(aTile.winLocation);
     }];
@@ -100,34 +100,33 @@ puzzle_node::puzzle_node(PZPuzzle *puzzle) :
     weight_ = manhattan_;
 }
 
-puzzle_node::puzzle_node(const puzzle_node *node, char empty_tile) :
+puzzle_node::puzzle_node(puzzle_node *node, uint8_t empty_tile) :
   previous_node_(node),
   move_(node->move_ + 1),
   empty_tile_(empty_tile),
-  refcount_(new char(0))
+  refcount_(0)
 {
     ::memmove(tiles_, node->tiles_, sizeof(tiles_));
     std::swap(tiles_[node->empty_tile_], tiles_[empty_tile_]);
     manhattan_ = calculate_manhatten();
     weight_ = move_ + manhattan_;
-    ++(*(previous_node_->refcount_));
+    ++(previous_node_->refcount_);
 }
 
 puzzle_node::~puzzle_node() {
     if (NULL != previous_node_) {
-        if (0 == --(*(previous_node_->refcount_))) {
+        if (0 == --(previous_node_->refcount_)) {
             delete previous_node_;
         }
     }
-    delete refcount_;
 }
 
-void puzzle_node::enqueue_neighbours(std::priority_queue<puzzle_node *, std::vector<puzzle_node *>, puzzle_node::comparator > *queue) const {
-    char previous_empty_tile = previous_node_->empty_tile_;
-    char empty_x = empty_tile_ % kPuzzleSize;
-    char empty_y = empty_tile_ / kPuzzleSize;
+void puzzle_node::enqueue_neighbours(std::priority_queue<puzzle_node *, std::vector<puzzle_node *>, puzzle_node::comparator > *queue)  {
+    uint8_t previous_empty_tile = previous_node_->empty_tile_;
+    uint8_t empty_x = empty_tile_ % kPuzzleSize;
+    uint8_t empty_y = empty_tile_ / kPuzzleSize;
     
-    char next_empty_tile = empty_tile_ - 1;
+    uint8_t next_empty_tile = empty_tile_ - 1;
     if (0 < empty_x && next_empty_tile != previous_empty_tile) {
         puzzle_node *node = new puzzle_node(this, next_empty_tile);
         queue->push(node);
@@ -156,15 +155,15 @@ void puzzle_node::enqueue_neighbours(std::priority_queue<puzzle_node *, std::vec
     }
 }
 
-char puzzle_node::tile_index(PZTileLocation location) const {
+uint8_t puzzle_node::tile_index(PZTileLocation location) const {
     return location.y * kPuzzleSize + location.x;
 }
 
-char puzzle_node::calculate_manhatten() const {
-    char result = 0;
+uint8_t puzzle_node::calculate_manhatten() const {
+    uint8_t result = 0;
     for (int x = 0; x < kPuzzleSize; x++) {
         for (int y = 0; y < kPuzzleSize; y++) {
-            char number = tiles_[x + y * kPuzzleSize];
+            uint8_t number = tiles_[x + y * kPuzzleSize];
             if ((kTilesCount - 1) != number) {
                 result += abs(x - (number % kPuzzleSize)) + abs(y - (number / kPuzzleSize));
             }
@@ -180,10 +179,11 @@ char puzzle_node::calculate_manhatten() const {
     
     std::priority_queue<puzzle_node *, std::vector<puzzle_node *>, puzzle_node::comparator > queue;
     puzzle_node *node = new puzzle_node(self);
+    //size_t size = sizeof(puzzle_node);
     queue.push(node);
     
     while (!queue.empty()) {
-        const puzzle_node *node = queue.top();
+        puzzle_node *node = queue.top();
 //        std::cout << "Popped Manhattan: " << (int)node->manhattan() << " weight: " << node->weight() << std::endl;
         
         if (node->is_win()) {
