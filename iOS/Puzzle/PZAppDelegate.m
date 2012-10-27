b3b866fd116db2d12e069c365d56647ac6d5fd25
eugenebut <eugenebut@Eugenes-MacBook-Pro.local>
2012-10-27 15:04:31 -0700

    Removed iPad support.

--- iOS/Puzzle/PZAppDelegate.m
+++ iOS/Puzzle/PZAppDelegate.m
@@ -16,15 +16,18 @@
     
     // Override point for customization after application launch.
     NSString *imageFileName = nil;
-    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
+//    TODO: use ~iphone, ~ipad extenstions for images
+//    No iPad support yet
+    
+//    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
         self.viewController = [[PZViewController alloc] initWithNibName:@"PZViewController_iPhone" bundle:nil];
         imageFileName = @"Tiles_iPhone";
-    }
-    else {
-        self.viewController = [[PZViewController alloc] initWithNibName:@"PZViewController_iPad" bundle:nil];
-        imageFileName = @"Tiles_iPad";
-    }
-    // TODO: use ~iphone, ~ipad extenstions for images
+//    }
+//    else {
+//        self.viewController = [[PZViewController alloc] initWithNibName:@"PZViewController_iPad" bundle:nil];
+//        imageFileName = @"Tiles_iPad";
+//    }
+    
     self.viewController.tilesImageFile = [[NSBundle mainBundle] pathForResource:imageFileName ofType:@"png"];
     
     self.window.rootViewController = self.viewController;
